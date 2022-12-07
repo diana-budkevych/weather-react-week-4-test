d43599c0abeb28a1b2e9ae463adb022f69f772c7
diana-budkevych <morituria@ukr.net>
2022-12-07 19:24:51 +0200

    Added h1, p and form from the React Homework Week 3 (axios search engine)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hi there</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Weather() {
+  let [city, setCity] = useState(null);
+  let [temperature, setTemperature] = useState(null);
+
+  let [humidity, setHumidity] = useState(null);
+  let [wind, setWind] = useState(null);
+
+  function showWeather(response) {
+    setTemperature(Math.round(response.data.main.temp));
+
+    setHumidity(Math.round(response.data.main.humidity));
+    setWind(response.data.wind.speed);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "4153903a26a56002551e2a95d2d2783b";
+    let units = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(url).then(showWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  if (temperature) {
+    return (
+      <div>
+        <form className="searchForm" onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter a city"
+            onChange={updateCity}
+          />
+          <input type="submit" value="Search" />
+        </form>
+        <ul className="form">
+          <li>City: {city}</li>
+          <li>Temperature: {temperature}℃</li>
+
+          <li>Humidity: {humidity}%</li>
+          <li>Wind: {wind} km/h</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <form className="searchForm" onSubmit={handleSubmit}>
+        <input type="search" placeholder="Enter a city" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <h1>Weather App</h1>
     <App />
+    <p>
+      <a href="https://github.com/diana-budkevych/weather-react-week-4-test">
+        {" "}
+        Open-source{" "}
+      </a>{" "}
+      code by Diana Budkevych
+    </p>
   </React.StrictMode>
 );
 
