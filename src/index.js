d72a1eab6b8b2568f467fd48e550e4a834a1617f
diana-budkevych <morituria@ukr.net>
2022-12-14 18:35:24 +0200

    Deleted mentions about App.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <div className="container">
       <h1>Weather App</h1>
-      <App />
+
       <Weather />
       <footer>
         <p>
