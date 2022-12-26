5d035b76873e098ab0a565a0a8f0a35535d5466b
diana-budkevych <morituria@ukr.net>
2022-12-26 17:26:23 +0200

    Deleted the icon

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./WeatherForecast.css";
 
-export default function WeatherForecast(props) {
+export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Thu</div>
-          <img src={props.data.iconUrl} alt="cloudy" />
+
           <div className="WeatherForecast-temperatures">
             <span className="WeatherForecast-temperature-max">19°</span>
             <span className="WeatherForecast-temperature-min">10°</span>
